3cf0cd08147e40e312b18186c055b82d0393ae7d
tiszaszaki <jobljr9717@gmail.com>
2021-08-26 20:48:16 +0200

    first hotfixes

--- src/app/ff-todo-list/ff-todo-list.component.ts
+++ src/app/ff-todo-list/ff-todo-list.component.ts
@@ -7,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FfTodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    for (let phase of this.phase_labels)
+    {
+      this.todo_list.push([]);
+      this.task_count.push(0);
+    }
+  }
 
-  phase = 0;
+  todo_list: Array<Array<Object>> = [];
+  task_count: Array<Number> = [];
+
+  todo_sorting_field = [];
+  todo_sorting_direction = [];
+
+  task_sorting_field = [];
+  task_sorting_direction = [];
+
+  todoSortingFields = [
+    ['name', 'Todo name'],
+    ['description', 'Todo description'],
+    ['descriptionLength', 'Todo description length'],
+    ['taskCount', 'Task count in Todo'],
+    ['dateCreated', 'Date of Todo created'],
+    ['dateModified', 'Date of Todo updated']
+  ];
+
+  taskSortingFields = [
+    ['name', 'Task name'],
+    ['done', 'Task checked']
+  ];
+
+  phase_labels = ['Backlog', 'In progress', 'Done'];
+
+  readonlyTodo = false;
 
   ngOnInit(): void {
+    this.todo_list[0].push(
+      {name:'Sonic'}
+    );
   }
 
 }
