69116af5657641b34724247aaa91d3152b277c7e
tiszaszaki <jobljr9717@gmail.com>
2022-05-29 20:23:35 +0200

    prepared for use with ff-todo-aspnet backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/ff-todo/'
+  //apiUrl: 'http://localhost:8080/ff-todo/' // Spring Boot
+  apiUrl: 'http://localhost:5257/' // ASP.NET Core
 };
