03b3b8c199a115243ea0c6c440eb1d8d80390f79
tiszaszaki <jobljr9717@gmail.com>
2022-06-19 20:00:35 +0200

    Test connection to backend and indicate status

--- src/app/ff-todo-backend-switch-form/ff-todo-backend-switch-form.component.ts
+++ src/app/ff-todo-backend-switch-form/ff-todo-backend-switch-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { Subscription } from 'rxjs';
+import { FfTodoAbstractRequestService } from '../ff-todo-abstract-request.service';
 import { FfTodoAlertService } from '../ff-todo-alert.service';
 import { FfTodoCommonService } from '../ff-todo-common.service';
 
@@ -15,13 +16,18 @@ export class FfTodoBackendSwitchFormComponent implements OnInit, OnDestroy {
   public backendSelected!: string;
   private backendSelectedListener!: Subscription;
 
+  public backendRefreshStatus!: Number;
+
   public backendIds!: string[];
 
   constructor(
       private common: FfTodoCommonService,
       private router: Router,
       private alertServ: FfTodoAlertService,
+      private todoServ: FfTodoAbstractRequestService,
       private cookies: CookieService) {
+    this.backendRefreshStatus = 2;
+
     this.backendIds = this.common.getBackendIds();
   }
 
@@ -31,16 +37,25 @@ export class FfTodoBackendSwitchFormComponent implements OnInit, OnDestroy {
 
   public switchBackend()
   {
+    this.backendRefreshStatus = 0;
     if (this.common.changeBackend(this.backendSelected))
     {
-      this.cookies.set(this.common.cookies.selectedBackend, this.backendSelected);
-      this.alertServ.addAlertMessage({type: 'success', message: `Successfully switched backend: (${this.getBackendName(this.backendSelected)}).`});
-      this.router.navigate(["/"]);
-      this.common.updateBoardList();
+      setTimeout(() =>
+      this.todoServ.getBoardIds().subscribe(() => {
+        this.cookies.set(this.common.cookies.selectedBackend, this.backendSelected);
+        this.alertServ.addAlertMessage({type: 'success', message: `Successfully switched to backend with ID (${this.backendSelected}): (${this.getBackendName(this.backendSelected)}).`});
+        this.router.navigate(["/"]);
+        this.common.updateBoardList();
+        this.backendRefreshStatus = 2;
+      }, error => {
+        this.alertServ.addAlertMessage({type: 'danger', message: `Failed to switch to backend with ID (${this.backendSelected}). See browser console for details.`});
+        this.backendRefreshStatus = 3;
+      }), 250);
     }
     else
     {
-      this.alertServ.addAlertMessage({type: 'danger', message: `Failed to switch to backend with index (${this.backendSelected}). See browser console for details.`});
+      this.alertServ.addAlertMessage({type: 'warning', message: `Backend with ID (${this.backendSelected}) does not exist.`});
+      this.backendRefreshStatus = 1;
     }
   }
 
