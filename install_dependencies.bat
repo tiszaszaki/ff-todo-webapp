e25620c7812a55ca165735fb453db178f6bd1a40
tiszaszaki <jobljr9717@gmail.com>
2022-07-13 16:29:13 +0200

    attempt to fix dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,3 +1,20 @@
-npm install angular-bootstrap-md
-npm install karma-edge-launcher
+npm config set fund false --location=global
+
+npm install --location=global npm-check-updates
+
+ncu -u
+
+npm install --location=global
+
+npm install --location=global @angular/cli
+
+npm install --location=global @angular/core
+npm install --location=global @angular/common
+npm install --location=global @angular/animations
+npm install --location=global @angular/forms
+npm install --location=global @angular/cdk
+
+npm install --location=global angular-bootstrap-md
+npm install --location=global karma-edge-launcher
+
 ng add @ng-bootstrap/ng-bootstrap
