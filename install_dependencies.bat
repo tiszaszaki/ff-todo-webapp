38938fbc1aee8dfdaabeb1c8798f596acf153392
tiszaszaki <jobljr9717@gmail.com>
2022-07-14 20:04:20 +0200

    workarounds in dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -4,7 +4,8 @@ npm install --location=global npm-check-updates
 
 ncu -u
 
-npm install --location=global
+npm install --location=global --force
+npm install --location=global --legacy-peer-deps
 
 npm install --location=global @angular/cli
 
@@ -14,7 +15,11 @@ npm install --location=global @angular/animations
 npm install --location=global @angular/forms
 npm install --location=global @angular/cdk
 
+npm install --location=global angular-in-memory-web-api
+npm install --location=global ngx-cookie-service
 npm install --location=global angular-bootstrap-md
 npm install --location=global karma-edge-launcher
+
+npm update
 
 ng add @ng-bootstrap/ng-bootstrap
